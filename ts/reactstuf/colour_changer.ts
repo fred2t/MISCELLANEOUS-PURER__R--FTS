458c4e9e9d835fa37157e0afd24997f7f176877a
Frederic Tu <69024184+khyreek@users.noreply.github.com>
2022-02-28 22:38:14 -0500

    Update colour_changer.ts

--- ts/reactstuf/colour_changer.ts
+++ ts/reactstuf/colour_changer.ts
@@ -1,1 +1,19 @@
-uhh
+function ColourChanger(): JSX.Element {
+	const [colourSwitch, setColourSwitch] = useState<boolean>(false)
+	
+	useEffect((): (() => void) | undefined => {
+		if (!colourSwitch) return
+		
+		// ty to whoever made this colours array, forgot the link now
+		const colours = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","DarkOrange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","RebeccaPurple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"]
+		const colourInterval: NodeJS.Timer = setInterval((): void => {
+			console.log(123)
+			document.body.style.background = colours[Math.floor(Math.random() * colours.length)]
+		}, 1000)
+		return (): void => clearInterval(colourInterval)
+	}, [colourSwitch])
+	
+	return <div>
+		<button onClick={(): void => setColourSwitch((prev: boolean): boolean => !prev)}>{colourSwitch ? 'turn off' : 'turn on'}</button>
+	</div>
+}
