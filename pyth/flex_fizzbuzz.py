f4ac7691efd4987ec43b1766916e2acdba608fcc
Frederic Tu <69024184+khyreek@users.noreply.github.com>
2022-10-07 20:24:00 -0400

    Create flex_fizzbuzz.py

---
+++ pyth/flex_fizzbuzz.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def __init__(self) -> None:
+        self.fizz_buzz_hash = {3: 'Fizz', 5: 'Buzz'}
+
+    def fizzBuzz(self, n: int) -> List[str]:
+        fb_arr: list[str] = []
+
+        for i in range(1, n + 1):
+            fb = ''
+
+            for interval_key, zzer in self.fizz_buzz_hash.items():
+                if (i % interval_key):
+                    fb += zzer
+
+            fb_arr.append(fb or str(i))
+
+        return fb_arr
+
+        pass
